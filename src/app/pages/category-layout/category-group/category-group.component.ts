dea891c7faab5a9ec0cbd3c15855e147fd6775bf
AntoinePoree <poree47@gmail.com>
2025-05-20 17:38:30 +0200

    chore: add maquette

--- src/app/pages/category-layout/category-group/category-group.component.ts
+++ src/app/pages/category-layout/category-group/category-group.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+// import { rxResource } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-category-group',
   standalone: true,
   imports: [],
   templateUrl: './category-group.component.html',
-  styleUrl: './category-group.component.scss'
+  styleUrl: './category-group.component.scss',
 })
 export class CategoryGroupComponent {
-
+  // public categoryGroup = rxResource({
+  //   defaultValue: [],
+  // });
 }
+
